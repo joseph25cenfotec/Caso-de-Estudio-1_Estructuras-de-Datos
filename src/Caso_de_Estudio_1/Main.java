a0ad7f835a6192b17e9792fd8cf3f7600e6d15a1
Joseph UM <joseph.ugalde.murillo@gmail.com>
2026-02-25 15:36:04 -0600

    Implementado Menu en main para interacción cómoda y dinámica con el programa

--- src/Caso_de_Estudio_1/Main.java
+++ src/Caso_de_Estudio_1/Main.java
@@ -6,41 +6,81 @@ public class Main {
 
     static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingrese una expresión aritmética: ");
-        String expresion = scanner.nextLine();
+        String expresion = "";
+        int opcion = 0;
 
-        Token top = construirLista(expresion);
-        PilaCaracteres pila = new PilaCaracteres();
-        Token actual = top;
-        boolean error = false;
 
-        while (actual != null) {
+        while (opcion != 3) {
+            opcion = menu(scanner, expresion);
 
-            char caracter = actual.getValor();
+            switch (opcion) {
+                case 1: {
+                    System.out.print("Ingrese una expresión aritmética: ");
+                    expresion = scanner.nextLine();
 
-            if (caracter == '(') {
-                pila.push(caracter);
-            } else if (caracter == ')') {
-                if (pila.isEmpty()) {
-                    System.out.println("ERROR: Paréntesis de cierre sin apertura.");
-                    error = true;
+                    if (expresion.isEmpty()) {
+                        System.out.println("ERROR: No se aceptan expresiones vacías");
+                    } else {
+                        System.out.println("Expresión ingresada con éxito");
+                    }
                     break;
-                } else {
-                    pila.pop();
+                }
+
+                case 2: {
+                    if (expresion.isEmpty()) {
+                        System.out.println("ERROR: No se puede analizar una expresión vacía");
+                        System.out.println("Por favor ingrese una expresión");
+                        break;
+                    }
+
+                    Token top = construirLista(expresion);
+                    PilaCaracteres pila = new PilaCaracteres();
+                    Token actual = top;
+                    boolean error = false;
+
+                    while (actual != null) {
+
+                        char caracter = actual.getValor();
+
+                        if (caracter == '(') {
+                            pila.push(caracter);
+                        } else if (caracter == ')') {
+                            if (pila.isEmpty()) {
+                                System.out.println("ERROR: Paréntesis de cierre sin apertura.");
+                                System.out.println("Faltante en la expresión: (");
+                                error = true;
+                                break;
+                            } else {
+                                pila.pop();
+                            }
+                        }
+                        actual = actual.siguiente;
+                    }
+
+                    if (!error) {
+                        if (!pila.isEmpty()) {
+                            System.out.println("ERROR: Hay paréntesis sin cerrar.");
+                            System.out.println("Faltante en la expresión: )");
+                        } else {
+                            System.out.println("Expresión válida.");
+                        }
+                    }
+
+                    break;
+                }
+
+                case 3: {
+                    System.out.println("SALIENDO DEL PROGRAMA...");
+                    break;
+                }
+
+                default: {
+                    System.out.print("Opción inválida, por favor ingrese otra opción");
                 }
             }
-            actual = actual.siguiente;
         }
 
-        if (!error) {
-            if (!pila.isEmpty()) {
-                System.out.println("ERROR: Hay paréntesis sin cerrar.");
-            } else {
-                System.out.println("Expresión válida.");
-            }
-        }
 
-        scanner.close();
     }
 
     public static Token construirLista(String expresion) {
@@ -56,5 +96,27 @@ public class Main {
         }
 
         return top;
+    }
+
+    public static int menu(Scanner scanner, String expresion) {
+        int opcion = 0;
+
+        System.out.println("--- Sistema de Análisis de Expresiones Aritméticas ---");
+
+        if (!expresion.isEmpty()) {
+            System.out.print("\n");
+            System.out.println("Expresión Ingresada: " + expresion);
+            System.out.print("\n");
+        }
+
+        System.out.println("Seleccione una opción:");
+        System.out.println("1. Ingresar Expresión");
+        System.out.println("2. Analizar");
+        System.out.println("3. Salir");
+        System.out.print("\n");
+        System.out.print("Ingrese una opción: ");
+        opcion = Integer.parseInt(scanner.nextLine());
+
+        return opcion;
     }
 }
